9bb5feb86d62196681a0675e4b11e5920ad2e323
Shawn Cockburn <shawn@Administrators-iMac.local>
2020-10-23 15:59:02 +0100

    Started work on ui

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1000,
+    minWidth: 800,
+    minHeight: 800,
     webPreferences: {
       nodeIntegration: true
     },
